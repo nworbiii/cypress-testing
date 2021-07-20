fb423a8f8524c7aad3b4389ecdc5902cfb219d34
Kyle Brown <kylealbrown@gmail.com>
2021-07-20 01:13:50 -0400

    add Cypress to netlify build

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 it('loads the homepage and navigates to about', () => {
-    cy.visit('public/index.html')
+    cy.visit('/')
     cy.contains('h1', 'This Page is Tested')
     cy.get('[data-test]').click()
     cy.contains('h1', 'About')
